ecc771357266def1c109b2e658d04acd1ff7665a
Enrique <enriquesamudio1999@gmail.com>
2023-01-20 17:02:15 -0300

    Email validator added

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
+const heroForm = document.getElementById('hero-form');
+const faqForm = document.getElementById('faq-form');
+
 const accordionItems = document.querySelectorAll('.faq-accordion__item');
 const accordionQuestions = document.querySelectorAll('.faq-accordion__question');
+
+const emailRE = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 document.addEventListener('DOMContentLoaded', () => initApp());
 
 const initApp = () => {
 
     accordionQuestions.forEach(accordionQuestion => accordionQuestion.addEventListener('click', toggleQuestion));
+
+    heroForm.addEventListener('submit', submitForm);
+    faqForm.addEventListener('submit', submitForm);
 
 }
 
@@ -26,5 +34,23 @@ const toggleQuestion = (e) => {
             accordionItem.classList.remove("faq-accordion__item--active");
         }
     });
+
+}
+
+const submitForm = (e) => {
+
+    e.preventDefault();
+    const emailInput = e.target.querySelector('input[type="email"]');
+
+    const email = emailInput.value.toLowerCase();
+
+    if (!email.match(emailRE)) {
+        emailInput.parentNode.classList.add('form__input-container--invalid');
+        emailInput.focus();
+        return;
+    } 
+
+    emailInput.parentNode.classList.remove('form__input-container--invalid');
+    emailInput.value = "";
 
 }
